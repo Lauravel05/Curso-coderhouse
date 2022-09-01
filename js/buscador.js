10460043aff27f0a0e02b281dfc7500e26724026
Laura Velandia <lauravelandia1@hotmail.com>
2022-09-01 08:41:49 -0500

    Funcionalidad de buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,3 +1,7 @@
+document.getElementById("icon-search").addEventListener("click", mostrarBuscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultarBuscador);
+document.getElementById("inputSearch").addEventListener("keyup", buscadorInterno);
+
 
 let barSearch = document.getElementById("ctn-bars-search");
 let coverSearch = document.getElementById("cover-ctn-search");
@@ -5,5 +9,30 @@ let inputSearch = document.getElementById("inputSearch");
 let boxSearch = document.getElementById("box-search");
 
 function mostrarBuscador(){
-    barSearch.style.top= "80px";
+    barSearch.style.top = "22%";
+    coverSearch.style.display = "block";
+    inputSearch.focus();
+}
+
+function ocultarBuscador(){
+    barSearch.style.top = "-22%";
+    coverSearch.style.display = "none";
+    inputSearch.value = "";
+}
+
+function buscadorInterno(){
+    let filter = inputSearch.value.toUpperCase();
+    let li = boxSearch.getElementsByTagName("li");
+    console.log(li)
+    for (i = 0; i < li.length; i++){
+        let a = li[i].getElementsByTagName("a");
+        let textValue = a[0].textContent || a[0].innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "";
+            boxSearch.style.display = "block"
+        }else{
+            li[i].style.display = "none";
+        }
+    }
 }
