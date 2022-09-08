7c2a947d40350adb2d5dfd5efd870f9bb5def75a
Laura Velandia <lauravelandia1@hotmail.com>
2022-09-07 21:17:05 -0500

    Agregando fetch

--- js/obtenerProductos.js
+++ js/obtenerProductos.js
@@ -1,40 +1,12 @@
-const productos = [
-    {
-        nombre: "Velón #8",
-        precio: "8.500",
-        id: 1,
-        img: "./images/producto1.jpeg",
-        link: "pages/vela8.html"
-    },
-    {
-        nombre: "Velón #9",
-        precio: "10.000",
-        id: 2,
-        img: "./images/producto2.jpeg",
-        link: "pages/vela9.html"
-    },
-    {
-        nombre: "Pebeteros",
-        precio: "8.500",
-        id: 3,
-        img: "./images/producto3.jpeg",
-        link: "pages/pebeteros.html"
-    },
-    {
-        nombre: "Aromatizadas",
-        precio: "4.500",
-        id: 4,
-        img: "./images/producto4.jpeg",
-        link: "pages/aromatizadas.html"
-    },
-];
-
 const containerDiv = document.querySelector(".contenedor")
 const carritoDiv = document.querySelector(".carrito")
-
+let productos = []
 let carrito = []
 
-function crearCards(){
+async function crearCards(){
+    productos = await fetch("../json/productos.json")
+    .then(result => result.json())
+    .then(data => {return data})
     productos.forEach(prod=>{
         containerDiv.innerHTML += `<div class="product text-center col-lg-3 col-md-4 col-12">
         <img class="img-fluid mb-3" src=${prod.img} alt="producto4">
